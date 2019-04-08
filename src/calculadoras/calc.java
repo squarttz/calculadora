2cde4c4bc8186737475914dea33e2af9b829cc7e
Adrian Casagrande <adriancasagrande1805@gmail.com>
2019-04-08 14:41:18 -0300

    Multi

--- src/calculadoras/calc.java
+++ src/calculadoras/calc.java
@@ -2,9 +2,17 @@ package calculadoras;
 
 import java.util.Scanner;  
 public class calc {  
-    /*Declaração dos métodos*/  
-    public int som(int num1, int num2) {
-        return num1 + num2;
-    }  
-   
+   /*Declaração dos métodos*/  
+   public int som(int num1, int num2) {
+       return num1 + num2;
+   }  
+ public int sub(int num1, int num2){
+       return num1 - num2;
+   }  
+   public int div(int num1,int num2){
+       return num1 / num2;
+   }  
+   public int mult(int num1, int num2){
+       return num1 * num2;
+   }  
 }
